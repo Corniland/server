fe0a8e35f02db6e993203bcf99dc74a03fb065b1
Pierre Demessence <pierre.demessence@gmail.com>
2020-09-26 21:28:38 +0200

    Init repo (#7)
    * initial boilerplate
    
    * api endpoints mockup
    
    * added vscode .gitignore
    
    * Added dotenv safe, eslint, prettier and changed package.json
    
    * Added github workflow
    
    * Added .vscode files
    
    * Better .gitignore
    
    * deleted yarn.lock
    
    * added _ to unused function parameter
    
    Co-authored-by: CowboyBebop <antonpirogov@mail.ru>

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    mongo: true,
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/eslint-recommended", "plugin:@typescript-eslint/recommended", "prettier/@typescript-eslint", "plugin:prettier/recommended"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  rules: {
+    // "no-console": process.env.NODE_ENV === "production" ? "error" : "warn",
+    // "no-debugger": process.env.NODE_ENV === "production" ? "error" : "warn",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        args: "after-used",
+        varsIgnorePattern: "^_",
+        argsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+      },
+    ],
+    "@typescript-eslint/no-explicit-any": [
+      "warn",
+      {
+        ignoreRestArgs: false,
+      },
+    ],
+    "@typescript-eslint/naming-convention": [
+      "error",
+      {
+        selector: "property",
+        format: ["camelCase"],
+      },
+    ],
+  },
+};
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require("dotenv-safe").config();
+
+import express from "express";
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/", (_req, res) => {
+  res.send("Wurks");
+});
+
+app.listen(PORT, () => console.log(`Server running at port: ${PORT}`));
