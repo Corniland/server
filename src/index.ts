8308871cbc625f764bf52530ae1e5bef32d63ccb
Pierre Demessence <pierre.demessence@gmail.com>
2020-09-26 16:40:46 +0200

    Added dotenv safe, eslint, prettier and changed package.json

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
-import express, {Application, Request, Response, NextFunction} from "express";
-//import cors from "cors";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require("dotenv-safe").config();
+
+import express from "express";
 
 const PORT = process.env.PORT || 5000;
 
-const app :Application = express();
+const app = express();
 
-app.get('/', (req:Request, res:Response, next:NextFunction) => {
-    res.send('Wurks');
+app.get("/", (req, res) => {
+  res.send("Wurks");
 });
-
 
 app.listen(PORT, () => console.log(`Server running at port: ${PORT}`));
