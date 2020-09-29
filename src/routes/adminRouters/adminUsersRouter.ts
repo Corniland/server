31d88f5114fc805726fb94bfe03ac3cddc136485
CowboyBebop <antonpirogov@mail.ru>
2020-09-29 19:35:29 +0300

    renamed adminUserRouter to adminUsersRouter to remove a code smell

--- src/routes/adminRouters/adminUsersRouter.ts
+++ src/routes/adminRouters/adminUsersRouter.ts
@@ -1,29 +1,29 @@
 import express from "express";
-const adminUserRouter = express.Router();
+const adminUsersRouter = express.Router();
 
 //TODO: Admin Authenticate
-adminUserRouter.get("/", (req, res) => {
+adminUsersRouter.get("/", (req, res) => {
   res.send("list of all users");
 });
 
 //TODO: Admin Authenticate
-adminUserRouter.get("/:userId", (req, res) => {
+adminUsersRouter.get("/:userId", (req, res) => {
   res.send("specific user data");
 });
 
 //TODO: Admin Authenticate
-adminUserRouter.delete("/:userId", (req, res) => {
+adminUsersRouter.delete("/:userId", (req, res) => {
   res.send("deleted a user");
 });
 
 //TODO: Admin Authenticate
-adminUserRouter.post("/:userId/ban", (req, res) => {
+adminUsersRouter.post("/:userId/ban", (req, res) => {
   res.send("banned a user");
 });
 
 //TODO: Admin Authenticate
-adminUserRouter.delete("/:userId/ban", (req, res) => {
+adminUsersRouter.delete("/:userId/ban", (req, res) => {
   res.send("unbanned a user");
 });
 
-export default adminUserRouter;
+export default adminUsersRouter;
