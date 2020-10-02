a79d5cb9d4e3229fe0e6c210651490a8a0ca3dce
Cowboy Bebop <33606633+CowboyBebop@users.noreply.github.com>
2020-10-03 01:41:58 +0300

    user registration (#19)
    * user registration
    
    * fixed codesmells
    
    * no message
    
    * no message

--- src/routes/auth/user.ts
+++ src/routes/auth/user.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { validateSignupData, validateLoginData } from "../../util/validators";
-//import { User } from "../models/user";
+import { User, UserModel } from "../../models/user";
+import bcrypt from "bcrypt";
 
 export const userAuthRouter = express.Router();
 
@@ -16,20 +17,41 @@ export interface UserLoginData {
 }
 
 //user register route
-userAuthRouter.post("/register", (req, res) => {
+userAuthRouter.post("/register", async (req, res) => {
   const newUser: UserRegisterData = { email: req.body.email, username: req.body.username, password: req.body.password };
 
   const { valid, errors } = validateSignupData(newUser);
 
   if (!valid) return res.status(400).json(errors);
 
-  //TODO: checking if user doesn't already exist in DB
-  //TODO: Gen salt and store it together with the hashed password
-  //TODO: add user to DB
+  //checking if user doesn't already exist in DB
+  const userUsernameDoc = await UserModel.findOne({ username: newUser.username }).exec();
+  const userEmailDoc = await UserModel.findOne({ email: newUser.email }).exec();
+  if (userUsernameDoc) return res.status(400).json({ username: "this username is already taken" });
+  if (userEmailDoc) return res.status(400).json({ email: "this email is already taken" });
+
+  // Gen salt and store it together with the hashed password
+  const userPasswordSalt = await bcrypt.genSalt(10);
+  const hashedPassword = <string>await bcrypt.hash(newUser.password, userPasswordSalt).catch((err) => console.log(err));
+
+  // Add user to DB
+  const _newUserModel = (
+    await UserModel.create({
+      email: newUser.email,
+      username: newUser.username,
+      password: hashedPassword,
+      password_salt: userPasswordSalt,
+      private_profile: false,
+      banned: false,
+    })
+  ).save();
+
+  res.status(200);
+  //TODO: Return auth token;
 });
 
 //user login route
-userAuthRouter.post("/login", (req, res) => {
+userAuthRouter.post("/login", async (req, res) => {
   const userLoginData: UserLoginData = { email: req.body.email, password: req.body.password };
 
   const { valid, errors } = validateLoginData(userLoginData);
@@ -37,11 +59,12 @@ userAuthRouter.post("/login", (req, res) => {
   if (!valid) return res.status(400).json(errors);
 
   //TODO: Authenticate user
+
   //TODO: Return auth token
 });
 
 //user whomai route
-userAuthRouter.post("/me", (req, res) => {
+userAuthRouter.post("/me", (_req, _res) => {
   //TODO: Decrypt JWT token to get some user data
   //TODO: look for the user in db
   //TODO  return data
