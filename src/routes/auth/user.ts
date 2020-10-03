fb7e759a9f665b5d30b89693917d72481b7cfc68
Cowboy Bebop <33606633+CowboyBebop@users.noreply.github.com>
2020-10-03 21:52:47 +0300

    Feature/jwt authentication (#20)
    * JWT authentication + login route
    
    * user whoami route

--- src/routes/auth/user.ts
+++ src/routes/auth/user.ts
@@ -1,7 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require("dotenv-safe").config();
+
 import express from "express";
 import { validateSignupData, validateLoginData } from "../../util/validators";
-import { User, UserModel } from "../../models/user";
+import { UserModel } from "../../models/user";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import authDataOnlyMiddleware from "../../middleware/auth/authDataOnlyMiddleware";
 
 export const userAuthRouter = express.Router();
 
@@ -46,8 +51,12 @@ userAuthRouter.post("/register", async (req, res) => {
     })
   ).save();
 
-  res.status(200);
-  //TODO: Return auth token;
+  // Return auth token;
+  let accessToken;
+  const TOKEN = process.env.ACCESS_TOKEN_SECRET;
+  TOKEN ? (accessToken = jwt.sign({ email: newUser.email, username: newUser.username }, TOKEN)) : console.log("No secret access token in env");
+
+  res.status(200).json({ jwt: `Bearer ${accessToken}` });
 });
 
 //user login route
@@ -60,21 +69,31 @@ userAuthRouter.post("/login", async (req, res) => {
 
   //TODO: Authenticate user
 
-  //TODO: Return auth token
+  //checking if user doesn't already exist in DB
+  const userEmailDoc = await UserModel.findOne({ email: userLoginData.email }).exec();
+  if (!userEmailDoc) return res.status(400).json({ login: "email or password is incorrect" });
+  else if (!(await userEmailDoc.checkPassword(userLoginData.password))) {
+    return res.status(400).json({ login: "email or password is incorrect" });
+  }
+
+  // Return auth token;
+  let accessToken;
+  const TOKEN = process.env.ACCESS_TOKEN_SECRET;
+  TOKEN ? (accessToken = jwt.sign({ email: userEmailDoc.email, username: userEmailDoc.username }, TOKEN)) : console.log("No secret access token in env");
+
+  res.status(200).json({ jwt: `Bearer ${accessToken}` });
 });
 
 //user whomai route
-userAuthRouter.post("/me", (_req, _res) => {
-  //TODO: Decrypt JWT token to get some user data
-  //TODO: look for the user in db
-  //TODO  return data
-  /*
-    {
-    "email": "foo@example.com",
-    "login": "FooBar",
-    "private_profile": false,
-    "liked_projects": ["5f6e96852f1bc609ad3c55de", "5f6e98d4c4bb195ebf77a6d2"],
-    "banned": false
-    }
-    */
+userAuthRouter.post("/me", authDataOnlyMiddleware, async (req, res) => {
+  const userDoc = await UserModel.findOne({ username: res.locals.user.username }).exec(); // look for the user in db
+
+  // return data
+  res.status(200).json({
+    email: userDoc?.email,
+    username: userDoc?.username,
+    private_profile: userDoc?.private_profile,
+    liked_projects: userDoc?.liked_projects,
+    banned: userDoc?.banned,
+  });
 });
