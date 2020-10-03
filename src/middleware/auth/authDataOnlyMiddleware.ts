1fe377cacb6c98666f83d6b256ad19f3aa1907a9
CowboyBebop <antonpirogov@mail.ru>
2020-10-03 21:43:49 +0300

    JWT authentication + login route

--- src/middleware/auth/authDataOnlyMiddleware.ts
+++ src/middleware/auth/authDataOnlyMiddleware.ts
@@ -1,10 +1,33 @@
 import { Request, Response } from "express";
-import { Next } from "compose-middleware";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require("dotenv-safe").config();
 
-const authDataOnlyMiddleware = (_req: Request, _res: Response, next: Next): void => {
+import { Next } from "compose-middleware";
+import jwt from "jsonwebtoken";
+
+const authDataOnlyMiddleware = async (req: Request, res: Response, next: Next) => {
+  // parsing JWT token.
+  const authHeader = req.headers["authorization"];
+  const userAccessToken = authHeader && authHeader?.split(" ")[1];
+
+  if (userAccessToken == null) return res.sendStatus(401);
+
+  const TOKEN = process.env.ACCESS_TOKEN_SECRET;
+
+  let user;
+
+  if (TOKEN === undefined) console.log("No secret access token in env");
+  else
+    try {
+      user = jwt.verify(userAccessToken, TOKEN);
+    } catch (err) {
+      console.log(err);
+      return res.sendStatus(403);
+    }
+
+  // storing user data in res.locals.user
+  res.locals.user = user;
   next();
-  //TODO: parsing JWT token.
-  //TODO: storing user data in res.locals.user
 };
 
 export default authDataOnlyMiddleware;
