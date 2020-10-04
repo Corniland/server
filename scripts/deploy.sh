90727659797a01c03a40ba443cfa1eca13eaf242
Pierre Demessence <pierre.demessence@gmail.com>
2020-10-04 22:17:46 +0200

    Trying something else

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd $DIR/..
 
-source /home/ubuntu/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 npm install --only=prod
 pm2 restart ecosystem.config.js
