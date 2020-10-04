891c9cc8ce1f0b335ac70a7c4622d8fd518cc300
Pierre Demessence <pierre.demessence@gmail.com>
2020-10-04 22:19:26 +0200

    Removed useless lines

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,6 @@ cd $DIR/..
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 npm install --only=prod
 pm2 restart ecosystem.config.js
