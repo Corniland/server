145156677b7bf4f12a403bf9d31fc7fa63f09090
Cowboy Bebop <33606633+CowboyBebop@users.noreply.github.com>
2020-10-11 23:29:34 +0300

    body parser middleware (#29)
    Co-authored-by: Pierre Demessence <pierre.demessence@gmail.com>

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@
 require("dotenv-safe").config();
 
 import express from "express";
+import bodyParser from "body-parser";
 
 import routes from "./routes/indexRoutes";
 
@@ -22,6 +23,9 @@ process.on("unhandledRejection", async (err) => {
 });
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(routes);
 
