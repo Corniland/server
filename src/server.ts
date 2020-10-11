6644d3d232779605057198be3d765fc177c30139
Pierre Demessence <pierre.demessence@gmail.com>
2020-10-11 22:04:11 +0200

    Added hooks to catch uncaught errors

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,20 @@ import express from "express";
 
 import routes from "./routes/indexRoutes";
 
+process.on("uncaughtException", async (err) => {
+  console.error(`Uncaught Exception: ${err.stack}`);
+  if (process.env.NODE_ENV === "production") return;
+  console.error(`Exiting with status 1 in 3s`);
+  setTimeout(() => process.exit(1), 3000);
+});
+process.on("unhandledRejection", async (err) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  console.error(`Unhandled Rejection: ${(err as any).stack || err}`);
+  if (process.env.NODE_ENV === "production") return;
+  console.error(`Exiting with status 1 in 3s`);
+  setTimeout(() => process.exit(1), 3000);
+});
+
 const app = express();
 
 app.use(routes);
