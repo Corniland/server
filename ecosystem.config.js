7af2f75656fddfd1fdc37ce1acd0c9c7afc31ff9
Pierre Demessence <pierre.demessence@gmail.com>
2020-10-11 22:00:55 +0200

    Added NODE_ENV to ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,9 @@ module.exports = {
     {
       name: "Corniland-Server",
       script: "./dist/server.js",
+      env: {
+        NODE_ENV: "production",
+      },
       exec_mode: "cluster",
       instances: "max",
     },
