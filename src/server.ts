a8a21ace1101957f4f45b25eed7a42c29b35a113
CowboyBebop <antonpirogov@mail.ru>
2020-10-11 23:00:52 +0300

    body parser middleware

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,16 @@
 require("dotenv-safe").config();
 
 import express from "express";
+import bodyParser from "body-parser";
 
 import routes from "./routes/indexRoutes";
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(routes);
 
